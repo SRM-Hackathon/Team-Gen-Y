1e43b2c35640fd76ff28a1ceec2edb3ea43bad03
saurabh13209 <agrawal99saurabh@gmail.com>
2019-10-11 18:55:44 +0530

    Home Developed

---
+++ Android/app/src/main/java/com/srm/srmhack/Fragment/DiscoverFragment.java
@@ -0,0 +1,22 @@
+package com.srm.srmhack.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.srm.srmhack.R;
+
+public class GeneratorFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = getLayoutInflater().inflate(R.layout.fragment_generator , container, true);
+        return view;
+    }
+}
