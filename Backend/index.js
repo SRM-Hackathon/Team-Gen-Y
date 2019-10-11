9dfacdcfa398464a5a423d99e8ea941fbcf9a08c
Atif Hossain <atif_hossain@live.com>
2019-10-11 20:29:29 +0530

    Switched to cloud DB

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongodb = require("mongodb");
 const bodyParser = require("body-parser");
@@ -6,18 +7,23 @@ const app = express();
 const router = express.Router();
 const sampleRoute = require("./apis/sampleRoute");
 
-const dbUrl = "mongodb://localhost:27017/SRM-Hack";
+const dbUrl = process.env.dbUrl;
 const mongoClient = mongodb.MongoClient;
 let line = () => {
   console.log("____________________________________________\n");
 };
+
+mongoClient.connect(dbUrl, (err, db) => {
+  console.log("Database connected");
+  line();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/", sampleRoute);
 
-const port = 3000;
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
